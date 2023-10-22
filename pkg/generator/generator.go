06762e85e076ad83de9e7dd5354706196166d929
Matverk <matverik0@gmail.com>
2023-10-22 19:58:06 +0300

    added generator

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -1,1 +1,12 @@
 package generator
+
+import (
+	"math"
+	"math/rand"
+)
+
+func generate(cPrice float64) (volume float64, price float64) {
+	volume = float64(rand.Int31n(5000-10)+10) + rand.Float64()
+	price = math.Abs(rand.NormFloat64()*0.05*cPrice + cPrice) // editable compression parameter
+	return
+}
