29f903893557fe7b624c50155f84fe0335be671d
mgodunow <mikhailgodunow@gmail.com>
2023-10-22 19:23:44 +0300

    Prototype of StockBook is created

--- internal/domain/stock-book/stock-book.go
+++ internal/domain/stock-book/stock-book.go
@@ -1,6 +1,15 @@
 package stockBook
 
 type StockBook struct {
-	
+	buy *map[string]int64
+	sell *map[string]int64
 }
 
+func New() *StockBook {
+	buy := make(map[string]int64)
+	sell := make(map[string]int64)
+	return &StockBook{
+		buy: &buy,
+		sell: &sell,
+	}
+}
