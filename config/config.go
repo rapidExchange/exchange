a6d13a7b77dfd39422c34536ae697d6bc66ccb35
mgodunow <mikhailgodunow@gmail.com>
2023-10-23 22:45:47 +0300

    Add config reader

--- config/config.go
+++ config/config.go
@@ -1,1 +1,27 @@
 package config
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	RedisUser string `mapstructure:"REDIS_USER"`
+	RedisPassword string `mapstructure:"REDIS_PASS"`
+}
+
+func LoadConfig(path string) (c Config, err error) {
+
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&c)
+
+	return
+}
