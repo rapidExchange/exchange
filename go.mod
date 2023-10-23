1f02c2d1ba0d11c0022a1772efebe8040b8b78d2
mgodunow <mikhailgodunow@gmail.com>
2023-10-23 23:03:22 +0300

    add app struct & client for redis DB

--- go.mod
+++ go.mod
@@ -5,11 +5,14 @@ go 1.20
 require github.com/spf13/viper v1.17.0
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/redis/go-redis/v9 v9.2.1 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,44 @@
 package app
+
+import (
+	"context"
+	"fmt"
+	"rapidEx/config"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/spf13/viper"
+)
+
+type app struct {
+	c 				config.Config
+	ctx				context.Context
+	redisClient		*redis.Client
+}
+
+func New() (*app, error) {
+	var c config.Config
+
+	if err := viper.Unmarshal(&c); err != nil {
+		return nil, err
+	}
+	var a app
+
+	if err := a.setRedisConn(); err != nil {
+		return nil, err
+	}
+
+	return &a, nil
+
+}
+
+func (a *app) setRedisConn() error {
+	opt, err := redis.ParseURL(fmt.Sprintf("redis://%s:%s@localhost:6379", a.c.RedisUser, a.c.RedisPassword))
+
+	if err != nil {
+		return err
+	}
+
+	a.redisClient = redis.NewClient(opt)
+
+	return nil
+} 
