8e5e74a5214cdaf660397fa9d03aaaf48e397208
mgodunov <mikhailgodunow@gmail.com>
2023-10-26 19:13:16 +0300

    Implementation of getPrice controller

--- internal/controllers/controllers.go
+++ internal/controllers/controllers.go
@@ -1,8 +1,24 @@
 package controllers
 
 import (
+	"io"
+	"net/http"
+	"strings"
+	"encoding/json"
+
 	"github.com/gofiber/fiber/v2"
 )
+
+//TODO: normal error handling
+
+type getTickerPriceRequest struct {
+	Symbol string `json:"symbol"`
+}
+
+type getTickerPriceBinanceResponse struct {
+	Symbol string `json:"symbol"`
+	Price string `json:"price"`
+}
 
 func home(c *fiber.Ctx) error {
 	return c.SendString("Start page")
@@ -14,6 +30,46 @@ func stocks(c *fiber.Ctx) error {
 
 func stockPair(c *fiber.Ctx) error {
 	return c.SendString("Pair of stocks trade page")
+}
+
+func panel(c *fiber.Ctx) error {
+	return c.SendString("Admin panel")
+}
+
+func getTickerPrice(c *fiber.Ctx) error {
+	getPrice := new(getTickerPriceRequest)
+
+	if err := c.BodyParser(getPrice); err != nil {
+		return err
+	}
+
+	url := "https://api.binance.com/api/v3/ticker/price?symbol=" + strings.ToUpper(strings.TrimSpace(getPrice.Symbol))
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	priceResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var binanceResp getTickerPriceBinanceResponse
+
+	err = json.Unmarshal(priceResp, &binanceResp)
+	if err != nil {
+		return err
+	}
+
+	return c.SendString(binanceResp.Price)
 }
 
 func login(c *fiber.Ctx) error {
@@ -29,5 +85,7 @@ func RegisterRoutes(app *fiber.App) {
 	app.Post("/login", login)
 	app.Get("/stocks/:pair", stocks)
 	app.Get("/stocks", stockPair)
+	app.Get("/panel", panel)
+	app.Post("/price", getTickerPrice)
 	app.Get("/", home)
 }
