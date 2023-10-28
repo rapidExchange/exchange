c9c02e95d200ec1cd82bb28c6fb6afeb0114e07c
mgodunov <mikhailgodunow@gmail.com>
2023-10-28 09:55:06 +0300

    Move generator to internal/

---
+++ internal/generator/generator.go
@@ -0,0 +1,40 @@
+package generator
+
+import (
+	"math"
+	"math/rand"
+
+	"rapidEx/internal/domain/stock"
+	"rapidEx/internal/domain/order"
+	"rapidEx/internal/stock-price-processor"
+)
+
+type Generator struct {
+}
+
+func New() *Generator {
+	return &Generator{}
+}
+
+func (g Generator) generate(cPrice float64) (volume float64, price float64) {
+	volume = float64(rand.Int31n(5000-10)+10) + rand.Float64()
+	price = math.Abs(rand.NormFloat64()*0.05*cPrice + cPrice) // editable compression parameter
+	return
+}
+
+func (g Generator) OrderGenerate(s *stock.Stock) order.Order {
+	volume, price := g.generate(s.Price)
+	stockProcessor := stockPriceProcessor.New()
+	prec := stockProcessor.PreciseAs(price)
+	price = stockPriceProcessor.New().Round(price, prec)
+
+	ord := order.Order{Ticker: s.Ticker, Quantity: volume, Price: price}
+
+	switch ord.Price > s.Price {
+	case true:
+		s.Stockbook.Sell[ord.Price] += ord.Quantity
+	default:
+		s.Stockbook.Buy[ord.Price] += ord.Quantity
+	}
+	return ord
+}
