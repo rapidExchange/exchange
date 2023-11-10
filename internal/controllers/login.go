3950e845ad7f8c4f5c3d6a8efd06c40866d8f1f6
mgodunov <mikhailgodunow@gmail.com>
2023-11-10 14:11:54 +0300

    Implement login & register endpoints

--- internal/controllers/login.go
+++ internal/controllers/login.go
@@ -1,9 +1,47 @@
 package controllers
 
 import (
+	"context"
+	"database/sql"
+
 	"github.com/gofiber/fiber/v2"
+
+	"rapidEx/internal/domain/user"
+	"rapidEx/internal/mysql-connect"
 )
 
+type loginRequest struct {
+	Email        string `json:"email"`
+	PasswordHash string `json:"password_hash"`
+}
+
 func login(c *fiber.Ctx) error {
-	return c.SendString("Login route")
+	loginReq := loginRequest{}
+	if err := c.BodyParser(&loginReq); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	mc, err := mysqlconnect.SetMysqlConnection()
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	userRepository := user.NewRepository(mc)
+	ifReg, err := registerCheck(loginReq.Email, userRepository)
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	if !ifReg {
+		return c.SendStatus(fiber.StatusNoContent)
+	}
+	return c.SendStatus(fiber.StatusOK)
+}
+
+func registerCheck(email string, userRepository user.Repository) (bool, error) {
+	_, err := userRepository.Get(context.Background(), email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
--- internal/controllers/register.go
+++ internal/controllers/register.go
@@ -1,9 +1,43 @@
 package controllers
 
 import (
+	"context"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+
+	"rapidEx/internal/domain/user"
+	mysqlconnect "rapidEx/internal/mysql-connect"
 )
 
+type registerRequest struct {
+	Email        string `json:"email"`
+	PasswordHash string `json:"password_hash"`
+}
+
 func register(c *fiber.Ctx) error {
-	return c.SendString("Register route")
+	registerReq := new(registerRequest)
+	if err := c.BodyParser(&registerReq); err != nil {
+		log.Println(err)
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	mc, err := mysqlconnect.SetMysqlConnection()
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	userRepository := user.NewRepository(mc)
+	b, err := registerCheck(registerReq.Email, userRepository)
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	if b {
+		return c.SendStatus(fiber.StatusConflict)
+	}
+	usr := user.New(registerReq.Email, registerReq.PasswordHash)
+	err = userRepository.Create(context.Background(), usr)
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	return c.SendStatus(fiber.StatusOK)
+
 }
