3713a2870722b5e63e7937b844c754a41d7cfce2
mgodunov <mikhailgodunow@gmail.com>
2024-04-28 21:04:20 +0300

    Remove unnecessary logic

--- internal/tickerStorage/ticker_storage.go
+++ internal/tickerStorage/ticker_storage.go
@@ -10,14 +10,13 @@ type TickerStorage interface {
 	GetTickers() []string
 	Find(ticker string) bool
 	Get(ticker string) (int, bool)
-	Refresh() TickerStorage
 }
 
 // tickerStorage is an implementation of singleton pattern for store all stock tickers
 // the key of map is a ticker, value is precision(used for round prices and orders)
 type tickerStorage struct {
 	TickersPrecision map[string]int
-	sync.RWMutex
+	sync.Mutex
 }
 
 func GetInstanse() TickerStorage {
@@ -35,18 +34,17 @@ func (t *tickerStorage) TickerAppend(ticker string, precision int) {
 
 func (t *tickerStorage) GetTickers() []string {
 	tickers := make([]string, 0)
-	t.RLock()
+	t.Lock()
+	defer t.Unlock()
 	for k := range t.TickersPrecision {
 		tickers = append(tickers, k)
 	}
-	t.RUnlock()
 	return tickers
 }
 
 // Find returns true if provided ticker is in the stirage, otherwise returns false
 func (t *tickerStorage) Find(ticker string) bool {
 	tickers := t.GetTickers()
-
 	for _, t:= range tickers {
 		if t == ticker {
 			return true
@@ -58,8 +56,4 @@ func (t *tickerStorage) Find(ticker string) bool {
 func(t *tickerStorage) Get(ticker string) (int, bool) {
 	prec, ok := t.TickersPrecision[ticker]
 	return prec, ok
-}
-
-func (t *tickerStorage) Refresh() TickerStorage {
-	return t
 }
