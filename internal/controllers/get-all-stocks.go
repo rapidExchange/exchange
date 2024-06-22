c43b1b890b90b3b1a4427d4be196febc61fd3152
mgodunov <mikhailgodunow@gmail.com>
2024-06-22 22:58:17 +0300

    Use service method stocks for getting all stocks

--- internal/controllers/get-all-stocks.go
+++ internal/controllers/get-all-stocks.go
@@ -1,47 +1,30 @@
 package controllers
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"log/slog"
-	"os"
-	redisconnect "rapidEx/internal/redis"
-	stockrepository "rapidEx/internal/repositories/stock-repository"
-	"rapidEx/internal/services/stock"
-	tickerstorage "rapidEx/internal/tickerStorage"
 	"time"
 
 	"github.com/gofiber/contrib/websocket"
 )
 
-func GetAllStocks(c *websocket.Conn) {
+// TODO: remove magic sleep timeout
+func (c *Controllers) GetAllStocks(ws *websocket.Conn) {
 	const op = "controllers.GetAllStocks"
 	defer func() {
-		err := c.Close()
+		err := ws.Close()
 		if err != nil {
 			log.Printf("%s: %v\n", op, err)
 		}
 		log.Printf("WS conneciton closed")
 	}()
-
-	tickers := tickerstorage.GetInstanse().GetTickers()
-
-	redisConneciton := redisconnect.MustConnect()
-	stockRepository := stockrepository.NewStockRepository(redisConneciton)
-	stockMonitor := stock.New(slog.New(slog.NewTextHandler(os.Stdout, nil)), stockRepository,
-		stockRepository, nil)
 	for {
-		allStocksResponse := make(map[string]float64)
-		for _, ticker := range tickers {
-			stock, err := stockMonitor.Stock(context.Background(), ticker)
-			if err != nil {
-				log.Println(fmt.Errorf("%s: %w", op, err))
-				continue
-			}
-			allStocksResponse[stock.Ticker] = stock.Price
+		allStocksResponse, err := c.stockService.Stocks(c.ctx)
+		if err != nil {
+			log.Printf("%s: %v\n", op, err)
+			ws.Close()
 		}
-		c.WriteJSON(allStocksResponse)
+		ws.WriteJSON(allStocksResponse)
 		time.Sleep(1 * time.Second)
 		fmt.Println(allStocksResponse)
 	}
